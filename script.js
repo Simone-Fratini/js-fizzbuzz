46892c99acfe7ff3ae471d7621d443813029ca6b
Simone-Fratini <simone@fratini.com>
2024-10-29 13:08:14 +0100

    aggiunte tutte le funzionalità delll'esercizio

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+const numeri = 100;
+
+for (let i = 1; i <= numeri; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+    } else if (i % 5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
